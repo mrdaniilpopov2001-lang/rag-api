a450ca7afa69fbe115d8317c7063cd19b0f1b961
mrdaniilpopov2001-lang <mrdaniilpopov2001@gmail.com>
2026-02-18 19:56:59 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
